6bb26552c9ce7886029c044907b3b3c5fe419dc2
Pau Marin <6287958+Pau-Marin@users.noreply.github.com>
2022-07-13 23:44:20 +0200

    Chainlink & oracles

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -3,10 +3,12 @@ pragma solidity ^0.8.8;
 
 contract FundMe {
 
+	uint256 public minimumUsd = 50 * 1e18;
+
 	function fund() public payable {
 		// Want to be able to set a minimum fund amount in USD
 		// 1. How do we send ETH to this contract?
-		require(msg.value > 1e18, "Didn't send enough!"); // 1e18 == 1 * 10 ** 18 == 1.000.000.000.000.000.000
+		require(getConversionRate(msg.value) >= minimumUsd, "Didn't send enougth!"); // 1e18 == 1 * 10 ** 18 == 1.000.000.000.000.000.000
 	}
 
 	// function withdraw() {}
