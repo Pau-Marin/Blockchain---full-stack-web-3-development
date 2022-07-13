630ff89648ccdc7de7e4375d4ce371c909d40616
Pau Marin <6287958+Pau-Marin@users.noreply.github.com>
2022-07-13 21:24:29 +0200

    Basic Solidity functions

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -8,5 +8,14 @@ pragma solidity ^0.8.8;
 
 contract SimpleStorage {
     // Variables are initialized to their null value by default (0 in ints)
-    uint256 favoriteNumber;
+    uint256 public favoriteNumber;
+
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+    }
+
+    // view & pure methods don't spend gas to run.
+    function retrieve() public view returns(uint256) {
+        return favoriteNumber;
+    }
 }
