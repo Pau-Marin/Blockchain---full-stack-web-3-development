7bd5898d74afec6b985c3414cbdc961dd85f4761
Pau Marin <6287958+Pau-Marin@users.noreply.github.com>
2022-07-13 21:16:16 +0200

    Welcome to Remix!

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,11 @@
+ // SPDX-License-Identifier: MIT
+
+/*
+ *  ^ indicates this versión and above. Ex: ^0.8.8 = from 0.8.8 to 0.8.99999999...
+ *  >= < indicates a range of versions. Ex: >= 0.8.8 <0.8.10  = from 0.8.8 to less than (not included) 0.8.10
+ */ 
+pragma solidity ^0.8.8;
+
+contract SimpleStorage {
+    
+}
