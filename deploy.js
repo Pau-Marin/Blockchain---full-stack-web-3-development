66424320a7be338d9676368382b0b9010bdac899
Pau Marin <6287958+Pau-Marin@users.noreply.github.com>
2022-07-14 18:22:52 +0200

    Environment variables

--- deploy.js
+++ deploy.js
@@ -1,15 +1,11 @@
 const ethers = require("ethers");
 const fs = require("fs");
+require("dotenv").config();
 
 async function main() {
   // http://127.0.0.1:7545
-  const provider = new ethers.providers.JsonRpcProvider(
-    "http://127.0.0.1:7545"
-  );
-  const wallet = new ethers.Wallet(
-    "ba42369584672d75f98596bcf0483602da7d30f82d37ff6fe9a75cffde596354",
-    provider
-  );
+  const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
   const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
   const binary = fs.readFileSync(
     "./SimpleStorage_sol_SimpleStorage.bin",
