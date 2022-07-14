cbcf8cc4bea73e31793adc9a32c98b14afd88edc
Pau Marin <6287958+Pau-Marin@users.noreply.github.com>
2022-07-14 17:57:05 +0200

    Introduction to Ethers.js

--- deploy.js
+++ deploy.js
@@ -1,4 +1,25 @@
-async function main() {}
+const ethers = require("ethers");
+const fs = require("fs");
+
+async function main() {
+  // http://127.0.0.1:7545
+  const provider = new ethers.providers.JsonRpcProvider(
+    "http://127.0.0.1:7545"
+  );
+  const wallet = new ethers.Wallet(
+    "ba42369584672d75f98596bcf0483602da7d30f82d37ff6fe9a75cffde596354",
+    provider
+  );
+  const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
+  const binary = fs.readFileSync(
+    "./SimpleStorage_sol_SimpleStorage.bin",
+    "utf8"
+  );
+  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+  console.log("Deploying, please wait...");
+  const contract = await contractFactory.deploy(); // STOP here! Wait for contract to deploy
+  console.log(contract);
+}
 
 main()
   .then(() => process.exit(0))
