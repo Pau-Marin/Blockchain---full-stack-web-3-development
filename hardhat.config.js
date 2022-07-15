833b47f42baab204c563c26d2464c2fa2a715fd1
Pau Marin <6287958+Pau-Marin@users.noreply.github.com>
2022-07-15 18:05:16 +0200

    Hardhat Fund Me

---
+++ hardhat.config.js
@@ -0,0 +1,36 @@
+require("dotenv").config()
+
+require("@nomiclabs/hardhat-etherscan")
+require("@nomiclabs/hardhat-waffle")
+require("hardhat-gas-reporter")
+require("solidity-coverage")
+require("hardhat-deploy")
+
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
+const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+    solidity: "0.8.8",
+    defaultNetwork: "hardhat",
+    networks: {
+        rinkeby: {
+            url: RINKEBY_RPC_URL,
+            accounts: [RINKEBY_PRIVATE_KEY],
+            chainId: 4,
+            blockConfirmations: 6,
+        },
+    },
+    gasReporter: {
+        enabled: false,
+        outputFile: "gas-report.txt",
+        noColors: true,
+        currency: "USD",
+        coinmarketcap: COINMARKETCAP_API_KEY,
+    },
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
+}
