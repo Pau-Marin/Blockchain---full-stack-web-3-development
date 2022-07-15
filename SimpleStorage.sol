7adf901a27bf66ec668a7b5f566eca2ef96babd3
Pau Marin <6287958+Pau-Marin@users.noreply.github.com>
2022-07-15 11:13:35 +0200

    Installed Hardhat

---
+++ SimpleStorage.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+
+/*
+ *  ^ indicates this versión and above. Ex: ^0.8.0 = from 0.8.0 to 0.8.99999999...
+ *  >= < indicates a range of versions. Ex: >= 0.8.8 <0.8.10  = from 0.8.8 to less than (not included) 0.8.10
+ */
+pragma solidity 0.8.7;
+
+contract SimpleStorage {
+    // Variables are initialized to their null value by default (0 in ints)
+    uint256 favoriteNumber;
+
+    // Binds a variable tipe to another one (string to uint256 in that example).
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    struct Person {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    // []: array can change size dinamically.
+    // [x]: array can only be x size.
+    Person[] public people;
+
+    function store(uint256 _favoriteNumber) public virtual {
+        favoriteNumber = _favoriteNumber;
+    }
+
+    // view & pure methods don't spend gas to run.
+    function retrieve() public view returns (uint256) {
+        return favoriteNumber;
+    }
+
+    // calldata: variable only exists temporarily during transaction, can't be modified.
+    // memory: variable only exists temporarily during transaction, can be modified.
+    // storage: variable exists outside of the transaction, can be modified.
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        people.push(Person(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
+}
