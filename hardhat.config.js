28ea95dc5a8bb1abde87b03e84d913693238ee23
Pau Marin <6287958+Pau-Marin@users.noreply.github.com>
2022-07-15 12:58:29 +0200

    Hardhat gas reporter

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox")
 require("dotenv").config()
 require("@nomiclabs/hardhat-etherscan")
+require("hardhat-gas-reporter")
+
 require("./tasks/block-number")
 
-const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
-const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL || "https://eth.rinkeby"
+const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY || "0xkey"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key"
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key"
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -24,5 +27,12 @@ module.exports = {
     solidity: "0.8.8",
     etherscan: {
         apiKey: ETHERSCAN_API_KEY,
+    },
+    gasReporter: {
+        enabled: false,
+        outputFile: "gas-report.txt",
+        noColors: true,
+        currency: "USD",
+        coinmarketcap: COINMARKETCAP_API_KEY,
     },
 }
