475c9efb3272bd0fc305523d4b27c9dccdf796c9
Pau Marin <6287958+Pau-Marin@users.noreply.github.com>
2022-07-15 12:10:17 +0200

    Interacting with contracts in Hardhat

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -9,9 +9,18 @@ async function main() {
     await simpleStorage.deployed()
     console.log(`Deployer contract to: ${simpleStorage.address}`)
     if (network.config.chainId === 4 && process.env.ETHERSCAN_API_KEY) {
+        console.log("Waiting for block txes...")
         await simpleStorage.deployTransaction.wait(6)
         await verify(simpleStorage.address, [])
     }
+
+    const currentValue = await simpleStorage.retrieve()
+    console.log(`Current value is: ${currentValue}`)
+
+    const transactionResponse = await simpleStorage.store(7)
+    await transactionResponse.wait(1)
+    const updatedValue = await simpleStorage.retrieve()
+    console.log(`Updated Value is: ${updatedValue}`)
 }
 
 async function verify(contractAddress, args) {
