647fca0b9bd10b9c9293da8079e6a7bf85e23dba
Pau Marin <6287958+Pau-Marin@users.noreply.github.com>
2022-07-17 00:16:53 +0200

    ES6 (Front End JS) vs NodeJS

--- HTML-FundMe/index.js
+++ HTML-FundMe/index.js
@@ -1,3 +1,14 @@
+// In Nodejs require()
+
+// In front-end Javascript you can't use require
+// import
+import { ethers } from "./ethers-5.6.esm.min.js"
+
+const connectButton = document.getElementById("connectButton")
+const fundButton = document.getElementById("fundButton")
+connectButton.onClick = connect
+fundButton.onClick = fund
+
 async function connect() {
     if (typeof window.ethereum !== "undefined") {
         try {
@@ -5,9 +16,14 @@ async function connect() {
         } catch (error) {
             console.log(error)
         }
-        document.getElementById("connectButton").innerHTML = "Connected!"
+        connectButton.innerHTML = "Connected!"
     } else {
-        document.getElementById("connectButton").innerHTML =
-            "Please install Metamask!"
+        connectButton.innerHTML = "Please install Metamask!"
+    }
+}
+
+async function fund(ethAmount) {
+    console.log(`Funding with ${ethAmount}...`)
+    if (typeof window.ethereum !== "undefined") {
     }
 }
