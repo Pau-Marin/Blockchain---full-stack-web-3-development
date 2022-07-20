93c30a144230219c6dbe0ecec923db2c1b594c10
Pau Marin <6287958+Pau-Marin@users.noreply.github.com>
2022-07-20 12:12:22 +0200

    Manual Header II

--- nextjs-smartcontract-lottery/components/ManualHeader.jsx
+++ nextjs-smartcontract-lottery/components/ManualHeader.jsx
@@ -1,6 +1,22 @@
 import { useMoralis } from "react-moralis"
 
 export default function ManualHeader() {
-    const { enableWeb3 } = useMoralis()
-    return <div>Hi from header!</div>
+    const { enableWeb3, account } = useMoralis()
+    return (
+        <div>
+            {account ? (
+                <div>
+                    Connected to {accoun.slice(0, 6)}...{accounts.slice(account.length - 4)}
+                </div>
+            ) : (
+                <button
+                    onClick={async () => {
+                        await enableWeb3
+                    }}
+                >
+                    Connect
+                </button>
+            )}
+        </div>
+    )
 }
